eb04fd317921e3c26beba913410457c04660a344
Artem <burenotti@gmail.com>
2022-05-10 22:54:53 +0300

    🐛 fixed bug with default settings

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import CodeEditor from "./components/CodeEditor";
 function App() {
 
     const url = new URL(document.location.href);
-    const editable = url.searchParams.get('editable') === 'true'
-    const runnable = url.searchParams.get('runnable') === 'true'
+    const editable = (url.searchParams.get('editable') ?? 'true') === 'true'
+    const runnable = (url.searchParams.get('runnable') ?? 'true') === 'true'
     const code = url.searchParams.get('code')
     const language = url.searchParams.get('language')
     const languageVer = url.searchParams.get('lang_version')
