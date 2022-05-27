fa1c06f1e73643be3afa1711913c78db0474eb33
Artem <burenotti@gmail.com>
2022-05-27 15:32:57 +0300

    ✨ Added useUserHook

---
+++ src/hooks/useUser.js
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+import User from "../services/user";
+
+export default function useUser() {
+    const [user, setUser] = useState(new User());
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        try {
+            const user = User.loadFromLocalstorage();
+            setUser(user);
+
+        } catch (e) {
+            setError(e);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (user.isAuthorized)
+            user.dumpToLocalStorage();
+    }, [user])
+    return [user, setUser, error];
+}
