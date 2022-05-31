d06b86a67a36c619f55939fac745c2318f92d6fb
Artem <burenotti@gmail.com>
2022-05-31 22:58:16 +0300

    ✨ Set snippet name as document.title

--- src/routes/snippet.jsx
+++ src/routes/snippet.jsx
@@ -2,16 +2,23 @@ import {useParams} from "react-router-dom";
 import useSnippet from "../hooks/useSnippet";
 import CodeEditor from "../components/CodeEditor";
 import Header from "../components/ui/Header";
+import {useEffect} from "react";
 
-export default function SnippetView() {
-
+export default function SnippetView({popup}) {
+    const [showPopup, ] = popup;
     const params = useParams();
     const [snippet, updateSnippet] = useSnippet(params.username, params.snippet);
+
+    useEffect(() => {
+        document.title = snippet ? snippet.name : "Code Flask";
+    }, [snippet])
+
+
     if (snippet)
         return (
             <div style={{padding: 15, position: "relative", width: "100%", height: "calc(100% - 90px)"}}>
                 <div style={{marginBottom: 20}}>
-                    <Header snippet={snippet} updateSnippet={updateSnippet}/>
+                    <Header showPopup={showPopup} snippet={snippet} updateSnippet={updateSnippet}/>
                 </div>
                 <CodeEditor
                     snippet={snippet}
