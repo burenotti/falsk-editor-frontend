a9fc05266b0ddf2b0bf01495d1ae2f29fea13fe8
Artem <burenotti@gmail.com>
2022-05-31 22:56:08 +0300

    ✨ Added callback on user clicks on UserCard

--- src/components/ui/UserCard.jsx
+++ src/components/ui/UserCard.jsx
@@ -1,10 +1,13 @@
 import styles from "./UserCard.module.css"
 
-export default function UserCard({user}) {
+export default function UserCard({user, showPopup}) {
     return (
         <div className={`${styles.userCard}`}>
             <img src={user.avatarURL} alt="avatar" className={styles.avatar}/>
-            <span className={styles.username}>{user.username}</span>
+            <span className={styles.username}
+                  onClick={() => showPopup(user.username)}>
+                {user.username}
+            </span>
         </div>
     )
 }
