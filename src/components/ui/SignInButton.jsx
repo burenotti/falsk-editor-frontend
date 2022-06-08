a6fbfb1e6509f23277433a8adf09f354c852d9fe
Artem <burenotti@gmail.com>
2022-06-08 11:49:53 +0300

    ✨ Added a className props to SignInButton and UserCard

--- src/components/ui/SignInButton.jsx
+++ src/components/ui/SignInButton.jsx
@@ -1,11 +1,11 @@
 import styles from "./SignInButton.module.css"
 
-export default function SignInButton() {
+export default function SignInButton({className}) {
     const authorizeUrl = new URL(process.env.REACT_APP_AUTHORIZATION_URL);
     const redirectURI = new URL('/authorize', process.env.REACT_APP_ORIGIN_URL)
 
     authorizeUrl.searchParams.append('redirect_uri', redirectURI.toString());
     return (
-        <a href={authorizeUrl.toString()} className={styles["sign-in"]}>Sign In</a>
+        <a href={authorizeUrl.toString()} className={`${styles["sign-in"]} ${className}`}>Sign In</a>
     )
 }
--- src/components/ui/UserCard.jsx
+++ src/components/ui/UserCard.jsx
@@ -1,8 +1,8 @@
 import styles from "./UserCard.module.css"
 
-export default function UserCard({user, showPopup}) {
+export default function UserCard({user, showPopup, className = ''}) {
     return (
-        <div className={`${styles.userCard}`}>
+        <div className={`${styles.userCard} ${className}`}>
             <img src={user.avatarURL} alt="avatar" className={styles.avatar}/>
             <span className={styles.username}
                   onClick={() => showPopup(user.username)}>
